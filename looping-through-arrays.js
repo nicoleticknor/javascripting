f0966b072013769ad70aee96d12da7da166d7f57
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-02-21 03:37:15 +0000

    updating looping-through-arrays.js

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,4 +1,5 @@
 let pets = ['cat', 'dog', 'rat']
 for (let i =0; pets[i] = pets[i] + 's', i < 3; i++)
 console.log(pets)
-// this is still fucked
+// how to log just the final iteration of the loop?
+// {something} after for loop and before console.log? 
